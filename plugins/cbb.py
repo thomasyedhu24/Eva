507c1f99a78f1ef36f52ea27da98902f16d6337b
thomasyedhu24 <thomasyedhu@gmail.com>
2024-08-23 11:52:08 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>🤖 My Name :</b> <a href='https://t.me/FileSharingXProBot'>File Sharing Bot</a> \n<b>📝 Language :</b> <a href='https://python.org'>Python 3</a> \n<b>📚 Library :</b> <a href='https://pyrogram.org'>Pyrogram {__version__}</a> \n<b>🚀 Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>📢 Channel :</b> <a href='https://t.me/Madflix_Bots'>Madflix Botz</a> \n<b>🧑‍💻 Developer :</b> <a href='tg://user?id={OWNER_ID}'>Jishu Developer</a>",
+            text = f"<b>★ My Name :</b> <a href='https://t.me/FileStoreXflixBot'>Fɪʟᴇ Sᴛᴏʀᴇ Bᴏᴛ</a> \n<b>★ Language :</b> <a href='https://python.org'>Python 3</a> \n<b>★ Library :</b> <a href='https://pyrogram.org'>Pyrogram {version}</a> \n<b>★ Server :</b> <a href='https://heroku.com'>Heroku</a> \n<b>★ Channel :</b> <a href='https://t.me/XFlixAdult'>XFlixAdult</a> \n<b>★ Channel :</b> <a href='https://t.me/Xeonflix'>Movies</a> \n<b>★ Developer :</b> <a href='tg://user?id={OWNER_ID}'>MrXeon</a>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
